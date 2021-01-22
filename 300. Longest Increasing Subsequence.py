09b78ffd21e1a65d8a41df2b4d4c2362682ca701
elegy <547442993@qq.com>
2021-01-22 17:37:11 +0900

    solved by DP,  O(n2)

--- 300. Longest Increasing Subsequence.py
+++ 300. Longest Increasing Subsequence.py
@@ -4,8 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        res=dict()
-        for i in nums:
+        dp=[0 for i in range(len(nums))]
+        for i in range(len(nums)):
+            m=0
+            for j in range(0,i):
+                if dp[j]>m and nums[j]<nums[i]:
+                    m=dp[j]
+            dp[i]=m+1
+        m = 1
+        for j in range(len(nums)):
+            if dp[j] > m :
+                m = dp[j]
+
+
+        return m
 
 
 
+
+r=Solution()
+print(r.lengthOfLIS([10,9,2,5,3,7,101,18]))

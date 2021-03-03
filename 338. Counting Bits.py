fae7881f192be91f3764d53ff2f2360d4f8409aa
Elegytsai <547442993@qq.com>
2021-03-03 11:41:54 +0900

    binary

---
+++ 338. Counting Bits.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        res = [-1 for i in range(num + 1)]
+        if num == 0:
+            return [0]
+        res[0] = 0
+        res[1] = 1
+        for i in range(len(res)):
+            if i == 0 or i == 1:
+                continue
+            else:
+                res[i] = res[i // 2] + i % 2
+
+        return res
+

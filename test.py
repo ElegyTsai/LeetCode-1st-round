b69f42fb83cc0bed70294e59d38822852f65a81b
elegy <547442993@qq.com>
2021-03-10 12:42:30 +0900

    BIT

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-a=["11","22"]
-k=10
-for i in range(k):
-    k+=1
-print(k)
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        sum = 0
+        stack = []
+        for i in range(len(height)):
+            while len(stack) != 0 and height[stack[-1]] < height[i]:
+                idx = stack.pop()
+                if len(stack)==0:
+                    break
+                sum += (min(height[stack[-1]],height[i]) - height[idx]) * (i - stack[-1] -1 )
+            stack.append(i)
+
+        return sum
+s=Solution()
+print(s.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
